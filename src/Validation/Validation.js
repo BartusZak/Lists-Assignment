bde3ca8fa6fe26762a074d4ce60a39f725d064cf
Bartłomiej Płoszyński <b.ploszynski@gmail.com>
2018-02-16 17:38:35 +0100

    "Text too short"/"Text long enough" depend length
    Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const validation = (props) => {
+    let validationMessage = "Tekst wystarczająco długi";
+
+    if (props.inputLength <= 5 ){
+        validationMessage = "Tekst za krótki";
+    }
     return (
         <div>
-            <p>{props.inputLength}</p>
+            <p>{validationMessage}</p>
+            {/* {
+                props.inputLength > 5 ?
+                    <p>Tekst wystarczająco długi</p> :
+                    <p>Tesk za krótki!</p>
+            } */}
         </div>
     );
 }
